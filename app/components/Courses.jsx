afb5f0674572d7b49b7feeb55c1c37625ca0de7a
Yared Kebede <135129440+Yareda21@users.noreply.github.com>
2024-02-02 10:10:27 +0300

    update

--- app/components/Courses.jsx
+++ app/components/Courses.jsx
@@ -32,28 +32,50 @@ export default function App() {
           }}
           navigation={true}
           modules={[Pagination, Navigation]}
-          className="mySwiper h-[100vh] w-full"
+          className="mySwiper h-[100vh] w-full max-h-[600px] overflow-auto sm:max-h-full"
         >
           <SwiperSlide>
             <section className="text-gray-400  body-font">
-              <div className="container px-5 py-24 mx-auto">
+              <div className="container py-24 px-3 block mx-auto">
                 <Image
                   src={h}
                   layout="fill"
                   objectFit="cover"
                   quality={100}
                   priority
-                  className=" z-0 "
+                  className=" z-[-1] "
                 />
 
-                <div className="flex flex-wrap  -m-4">
-                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                <div className="flex flex-wrap gap-4 justify-center max-h-[600px] overflow-auto sm:max-h-full">
+                  <div className="lg:w-[22%] md:w-[45%] w-full border-2 border-blue-400 rounded-lg backdrop-blur-lg">
                     <a className="block relative h-48 rounded overflow-hidden">
                       <Image
                         width={300}
                         height={300}
                         alt="ecommerce"
-                        className="object-cover object-center w-full h-full block"
+                        className="object-cover object-center w-full h-full block rounded-lg"
+                        src={tenor}
+                      />
+                    </a>
+                    <div className="flex flex-col z-1 mt-4">
+                      <h3 className="text-white text-xl tracking-widest title-font mb-1">
+                        CATEGORY
+                      </h3>
+                      <h2 className="text-white title-font text-sm font-medium">
+                        The Catalyzer
+                      </h2>
+                      <button className="flex mx-auto my-3 text-white bg-indigo-500 border-0 py-2 px-8 hover:bg-indigo-600 rounded text-lg">
+                        Read More ...
+                      </button>
+                    </div>
+                  </div>
+                  <div className="lg:w-[22%] md:w-[45%] w-full border-2 border-blue-400 rounded-lg ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      <Image
+                        width={300}
+                        height={300}
+                        alt="ecommerce"
+                        className="object-cover object-center w-full h-full block rounded-lg"
                         src={tenor}
                       />
                     </a>
@@ -67,77 +89,17 @@ export default function App() {
                       <p className="mt-1">$16.00</p>
                     </div>
                   </div>
-                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                  <div className="lg:w-[22%] md:w-[45%] w-full border-2 border-blue-400 rounded-lg ">
                     <a className="block relative h-48 rounded overflow-hidden">
                       <Image
                         width={300}
                         height={300}
                         alt="ecommerce"
-                        className="object-cover object-center w-full h-full block"
+                        className="object-cover object-center w-full h-full block rounded-lg"
                         src={tenor}
                       />
                     </a>
-                    <div className="mt-4">
-                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                        CATEGORY
-                      </h3>
-                      <h2 className="text-white title-font text-lg font-medium">
-                        Shooting Stars
-                      </h2>
-                      <p className="mt-1">$21.15</p>
-                    </div>
-                  </div>
-                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                    <a className="block relative h-48 rounded overflow-hidden">
-                      <Image
-                        width={300}
-                        height={300}
-                        alt="ecommerce"
-                        className="object-cover object-center w-full h-full block"
-                        src={tenor}
-                      />
-                    </a>
-                    <div className="mt-4">
-                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                        CATEGORY
-                      </h3>
-                      <h2 className="text-white title-font text-lg font-medium">
-                        Neptune
-                      </h2>
-                      <p className="mt-1">$12.00</p>
-                    </div>
-                  </div>
-                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                    <a className="block relative h-48 rounded overflow-hidden">
-                      <Image
-                        width={300}
-                        height={300}
-                        alt="ecommerce"
-                        className="object-cover object-center w-full h-full block"
-                        src={tenor}
-                      />
-                    </a>
-                    <div className="mt-4">
-                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                        CATEGORY
-                      </h3>
-                      <h2 className="text-white title-font text-lg font-medium">
-                        The 400 Blows
-                      </h2>
-                      <p className="mt-1">$18.40</p>
-                    </div>
-                  </div>
-                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                    <a className="block relative h-48 rounded overflow-hidden">
-                      <Image
-                        width={300}
-                        height={300}
-                        alt="ecommerce"
-                        className="object-cover object-center w-full h-full block"
-                        src={tenor}
-                      />
-                    </a>
-                    <div className="mt-4">
+                    <div className="flex flex-col z-1 mt-4">
                       <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
                         CATEGORY
                       </h3>
@@ -147,64 +109,104 @@ export default function App() {
                       <p className="mt-1">$16.00</p>
                     </div>
                   </div>
-                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                  <div className="lg:w-[22%] md:w-[45%] w-full border-2 border-blue-400 rounded-lg ">
                     <a className="block relative h-48 rounded overflow-hidden">
                       <Image
                         width={300}
                         height={300}
                         alt="ecommerce"
-                        className="object-cover object-center w-full h-full block"
+                        className="object-cover object-center w-full h-full block rounded-lg"
                         src={tenor}
                       />
                     </a>
-                    <div className="mt-4">
+                    <div className="flex flex-col z-1 mt-4">
                       <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
                         CATEGORY
                       </h3>
                       <h2 className="text-white title-font text-lg font-medium">
-                        Shooting Stars
+                        The Catalyzer
                       </h2>
-                      <p className="mt-1">$21.15</p>
+                      <p className="mt-1">$16.00</p>
                     </div>
                   </div>
-                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                  <div className="lg:w-[22%] md:w-[45%] w-full border-2 border-blue-400 rounded-lg ">
                     <a className="block relative h-48 rounded overflow-hidden">
                       <Image
                         width={300}
                         height={300}
                         alt="ecommerce"
-                        className="object-cover object-center w-full h-full block"
+                        className="object-cover object-center w-full h-full block rounded-lg"
                         src={tenor}
                       />
                     </a>
-                    <div className="mt-4">
+                    <div className="flex flex-col z-1 mt-4">
                       <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
                         CATEGORY
                       </h3>
                       <h2 className="text-white title-font text-lg font-medium">
-                        Neptune
+                        The Catalyzer
                       </h2>
-                      <p className="mt-1">$12.00</p>
+                      <p className="mt-1">$16.00</p>
                     </div>
                   </div>
-                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                  <div className="lg:w-[22%] md:w-[45%] w-full border-2 border-blue-400 rounded-lg ">
                     <a className="block relative h-48 rounded overflow-hidden">
                       <Image
                         width={300}
                         height={300}
                         alt="ecommerce"
-                        className="object-cover object-center w-full h-full block"
+                        className="object-cover object-center w-full h-full block rounded-lg"
                         src={tenor}
                       />
                     </a>
-                    <div className="mt-4">
+                    <div className="flex flex-col z-1 mt-4">
                       <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
                         CATEGORY
                       </h3>
                       <h2 className="text-white title-font text-lg font-medium">
-                        The 400 Blows
+                        The Catalyzer
                       </h2>
-                      <p className="mt-1">$18.40</p>
+                      <p className="mt-1">$16.00</p>
+                    </div>
+                  </div>
+                  <div className="lg:w-[22%] md:w-[45%] w-full border-2 border-blue-400 rounded-lg ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      <Image
+                        width={300}
+                        height={300}
+                        alt="ecommerce"
+                        className="object-cover object-center w-full h-full block rounded-lg"
+                        src={tenor}
+                      />
+                    </a>
+                    <div className="flex flex-col z-1 mt-4">
+                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                        CATEGORY
+                      </h3>
+                      <h2 className="text-white title-font text-lg font-medium">
+                        The Catalyzer
+                      </h2>
+                      <p className="mt-1">$16.00</p>
+                    </div>
+                  </div>
+                  <div className="lg:w-[22%] md:w-[45%] w-full border-2 border-blue-400 rounded-lg ">
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      <Image
+                        width={300}
+                        height={300}
+                        alt="ecommerce"
+                        className="object-cover object-center w-full h-full block rounded-lg"
+                        src={tenor}
+                      />
+                    </a>
+                    <div className="flex flex-col z-1 mt-4">
+                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                        CATEGORY
+                      </h3>
+                      <h2 className="text-white title-font text-lg font-medium">
+                        The Catalyzer
+                      </h2>
+                      <p className="mt-1">$16.00</p>
                     </div>
                   </div>
                 </div>
