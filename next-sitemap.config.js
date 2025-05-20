259b28d65a8d00e38eabcd8bac7b3075d2453887
Yareda21 <135129440+Yareda21@users.noreply.github.com>
2025-05-20 20:47:12 +0300

    great job

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: "https://www.yared-coding.vercel.app", // Replace with your website URL
+    siteUrl: "https://yared-coding.vercel.app", // Replace with your website URL
     generateRobotsTxt: true, // (optional) Generate robots.txt
     changefreq: "daily", // How often content changes (can be 'weekly' or 'monthly' too)
     priority: 0.9, // Set priority of pages (between 0.1 and 1.0)
