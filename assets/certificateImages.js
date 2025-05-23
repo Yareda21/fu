846b238c25b2b903fde2eadeccf2e666c38dbfa6
Yareda21 <135129440+Yareda21@users.noreply.github.com>
2025-05-23 22:34:14 +0300

    updated

--- assets/certificateImages.js
+++ assets/certificateImages.js
@@ -1,5 +1,7 @@
 export const certificateImages = [
+    "https://res.cloudinary.com/dlomcic7f/image/upload/v1748028585/AiCE_Certificate_x72sso.png",
     "https://res.cloudinary.com/dlomcic7f/image/upload/v1729169513/fu%20project%20data/Prog_klnuxf.png",
     "https://res.cloudinary.com/dlomcic7f/image/upload/v1729169513/fu%20project%20data/harvard_g4auuj.png",
     "https://res.cloudinary.com/dlomcic7f/image/upload/v1729169512/fu%20project%20data/DA_bpsvme.png",
+    "https://res.cloudinary.com/dlomcic7f/image/upload/v1732112628/personal/ALX_final_Certificate_gl0pzi.png",
 ];
